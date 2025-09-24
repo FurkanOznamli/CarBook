9501f9841d3cce60d8ae24b520a7d61fb0e6e2b4
FurkanOznamli <furkanoznamli10@gmail.com>
2025-09-24 18:49:20 +0300

    Feat(Entity):Created Entities

---
+++ Core/CarBook.Domain/Entities/CarPricing.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Domain.Entities
+{
+    public class CarPricing
+    {
+        public int CarPricingID { get; set; }
+        public string CarID { get; set; }
+        public Car Car { get; set; }
+        public string PricingID { get; set; }
+        public Pricing Pricing { get; set; }
+        public Decimal Amount { get; set; } 
+    }
+}
