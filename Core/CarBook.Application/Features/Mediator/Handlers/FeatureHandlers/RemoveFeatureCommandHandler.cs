de4b89f32e70c8de065d215b43153b06ccc100a3
FurkanOznamli <furkanoznamli10@gmail.com>
2025-10-01 22:02:05 +0300

    feat: Added Mediator, Handler, API and test implementations for Feature, FooterAddress, Location, Pricing, Service and SocialMedia

---
+++ Core/CarBook.Application/Features/Mediator/Handlers/FeatureHandlers/RemoveFeatureCommandHandler.cs
@@ -0,0 +1,28 @@
+﻿using CarBook.Application.Features.Mediator.Commands.FeatureCommands;
+using CarBook.Application.Interfaces;
+using CarBook.Domain.Entities;
+using MediatR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CarBook.Application.Features.Mediator.Handlers.FeatureHandlers
+{
+    public class RemoveFeatureCommandHandler : IRequestHandler<RemoveFeatureCommand>
+    {
+        private readonly IRepository<Feature> repository;
+
+        public RemoveFeatureCommandHandler(IRepository<Feature> repository)
+        {
+            this.repository = repository;
+        }
+
+        public async Task Handle(RemoveFeatureCommand request, CancellationToken cancellationToken)
+        {
+            var value = await repository.GetByIdAsync(request.Id);
+            await repository.RemoveAsync(value);
+        }
+    }
+}
