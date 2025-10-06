a2934e848abe7c9c5a6eb346db9d96dc1957713c
FurkanOznamli <furkanoznamli10@gmail.com>
2025-10-06 15:51:48 +0300

    Implemented About page consume logic, added JSON visualizer, sample dataset, and Become a Driver section.

---
+++ Frontends/CarBook.WebUI/ViewComponents/TestimonialViewComponents/_TestimonialComponentPartial.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace CarBook.WebUI.ViewComponents.TestimonialViewComponents
+{
+    public class _TestimonialComponentPartial : ViewComponent
+    {
+        private readonly IHttpClientFactory _httpClientFactory;
+
+        public _TestimonialComponentPartial(IHttpClientFactory httpClientFactory)
+        {
+            _httpClientFactory = httpClientFactory;
+        }
+
+        public async Task<IViewComponentResult> InvokeAsync()
+        {
+            var client = _httpClientFactory.CreateClient();
+            var responseMessage  = await client.GetAsync("http://localhost:5278/api/Testimonial"); 
+        }
+    }
+}
